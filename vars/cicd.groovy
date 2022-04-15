14be9621a0ca7ec31dc7624e4728eca4cae13917
SAGAR SONAWANE <103016750+SAGARSGS@users.noreply.github.com>
2022-04-15 18:19:01 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,4 +10,8 @@ def newDeploy("ip")
 {
     scp /home/ubuntu/.jenkins/workspace/SharedLibrary ubuntu@${"ip"}:/var/libltomcat9/webapps/sagar.war
 }
+def newTest("appname")
+{
+     sh 'java -jar /home/ubuntu/.jenkins/workspace/${"appname"}/testing.jar'
+}
 
