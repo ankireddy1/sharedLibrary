c75098d00befed85f28ace3bc0d26f18a895e073
SAGAR SONAWANE <103016750+SAGARSGS@users.noreply.github.com>
2022-04-15 18:06:06 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,9 +1,13 @@
-def newGit(repo)
+def newGit("repo")
 {
      git ${"repo"}
 }
 def newMaven()
 {
     sh 'mvn package'
+}
+def newDeploy("ip")
+{
+    scp /home/ubuntu/.jenkins/workspace/SharedLibrary ubuntu@${"ip"}:/var/libltomcat9/webapps/sagar.war
 }
 
